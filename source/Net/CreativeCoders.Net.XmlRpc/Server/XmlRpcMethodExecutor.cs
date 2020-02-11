8879c737a777e3db7390d1d4f8b7eb79f784bfbe
Chris <chris@creativecoders.com>
2020-02-11 18:57:19 +0100

    Convert method parameters

--- source/Net/CreativeCoders.Net.XmlRpc/Server/XmlRpcMethodExecutor.cs
+++ source/Net/CreativeCoders.Net.XmlRpc/Server/XmlRpcMethodExecutor.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using CreativeCoders.Core;
 using CreativeCoders.Net.XmlRpc.Definition;
 using CreativeCoders.Net.XmlRpc.Model;
 using CreativeCoders.Net.XmlRpc.Model.Values;
+using CreativeCoders.Net.XmlRpc.Model.Values.Converters;
 
 namespace CreativeCoders.Net.XmlRpc.Server
 {
@@ -31,12 +33,30 @@ namespace CreativeCoders.Net.XmlRpc.Server
                 throw new MissingMethodException();
             }
 
-            var parameters = methodCall.Parameters.Select(p => p.Data).ToArray();
 
-            return methodRegistration.Method.Invoke(methodRegistration.Target,
-                methodRegistration.Method.GetParameters().Any()
-                    ? parameters
-                    : new object[0]);
+            var parameters = GetParameters(methodRegistration.Method.GetParameters(), methodCall.Parameters.ToArray());
+
+            return methodRegistration.Method.Invoke(methodRegistration.Target, parameters);
+        }
+
+        private static object[] GetParameters(IReadOnlyList<ParameterInfo> parameterInfos, IReadOnlyCollection<XmlRpcValue> parameters)
+        {
+            if (!parameterInfos.Any())
+            {
+                return new object[0];
+            }
+
+            if (parameters.Count != parameterInfos.Count)
+            {
+                throw new TargetParameterCountException(
+                    $"Xml Rpc method parameter count mismatch. Method parameter count ({parameterInfos.Count}) != calling parameter count ({parameters.Count})");
+            }
+            
+            var converter = new XmlRpcValueToDataConverter();
+
+            return parameters
+                .Select((x, index) => converter.Convert(x, parameterInfos[index].ParameterType))
+                .ToArray();
         }
 
         [XmlRpcMethod("listMethods")]
