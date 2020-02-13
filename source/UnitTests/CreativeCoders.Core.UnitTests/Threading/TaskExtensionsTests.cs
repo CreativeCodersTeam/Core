762a53038466f8d09eda9936b4fd6addac4ef3c1
darthsharp <48331467+darthsharp@users.noreply.github.com>
2020-02-13 20:36:23 +0100

    Add support for async methods registration with return type Task<>

---
+++ source/UnitTests/CreativeCoders.Core.UnitTests/Threading/TaskExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using CreativeCoders.Core.Threading;
+using Xunit;
+
+namespace CreativeCoders.Core.UnitTests.Threading
+{
+    public class TaskExtensionsTests
+    {
+        [Fact]
+        public async Task ToTask()
+        {
+            const int value = 1234;
+            
+            var task = (Task) GetIntValue(value);
+
+            var result = await task.ToTask<int>();
+
+            Assert.IsType<int>(result);
+            Assert.Equal(value, result);
+        }
+
+        private static async Task<T> GetIntValue<T>(T value)
+        {
+            await Task.Delay(500);
+            
+            return value;
+        } 
+    }
+}
