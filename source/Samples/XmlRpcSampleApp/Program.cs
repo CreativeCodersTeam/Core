ce9c2b2733359c26bd1f9b57c9638928bc961b12
darthsharp <48331467+darthsharp@users.noreply.github.com>
2020-02-14 19:11:11 +0100

    Use StartAsync in AspNetCoreWebHost instead of RunAsync, which is blocking till host exits. Added sample program for XML RPC.

---
+++ source/Samples/XmlRpcSampleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace XmlRpcSampleApp
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class Program
+    {
+        public static async Task Main()
+        {
+            await new XmlRpcSample().RunAsync();
+            
+            Console.WriteLine("Press key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
