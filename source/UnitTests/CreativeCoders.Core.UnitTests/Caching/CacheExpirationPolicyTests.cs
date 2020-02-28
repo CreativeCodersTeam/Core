a328d84af335f18d8c6ac109d8b09b714255a345
darthsharp <48331467+darthsharp@users.noreply.github.com>
2020-02-28 19:03:01 +0100

    New method for retrieving a cache entry

---
+++ source/UnitTests/CreativeCoders.Core.UnitTests/Caching/CacheExpirationPolicyTests.cs
@@ -0,0 +1,27 @@
+﻿using CreativeCoders.Core.Caching;
+using Xunit;
+
+namespace CreativeCoders.Core.UnitTests.Caching
+{
+    public class CacheExpirationPolicyTests
+    {
+        [Fact]
+        public void CheckIsExpired_FuncReturnsFalse_ReturnsFalse()
+        {
+            var expirationPolicy = new CacheExpirationPolicy(() => false);
+            
+            Assert.False(expirationPolicy.CheckIsExpired());
+        }
+        
+        [Fact]
+        public void CheckIsExpired_FuncReturnsTrue_ReturnsTrue()
+        {
+            var expirationPolicy = new CacheExpirationPolicy(() => true);
+            
+            Assert.True(expirationPolicy.CheckIsExpired());
+        }
+        
+        // [Fact]
+        // public void 
+    }
+}
