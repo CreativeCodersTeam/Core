a3bdcff6234686607712153cfe022f5a670d6cae
darthsharp <48331467+darthsharp@users.noreply.github.com>
2020-03-01 19:24:25 +0100

    Change expiration policy .

--- source/Core/CreativeCoders.Core/Caching/ICacheExpirationPolicy.cs
+++ source/Core/CreativeCoders.Core/Caching/ICacheExpirationPolicy.cs
@@ -1,6 +1,9 @@
-﻿namespace CreativeCoders.Core.Caching {
-    public interface ICacheExpirationPolicy
-    {
-        bool CheckIsExpired();
+﻿using System;
+
+namespace CreativeCoders.Core.Caching {
+    public interface ICacheExpirationPolicy {
+        CacheExpirationMode ExpirationMode { get; }
+        DateTime AbsoluteDateTime { get; }
+        TimeSpan SlidingTimeSpan { get; }
     }
 }
