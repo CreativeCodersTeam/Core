032652ca65d696737b2c924a2e4d81d73e9da31d
darthsharp <48331467+darthsharp@users.noreply.github.com>
2020-04-30 21:44:01 +0200

    Add new TR64 method for retrieving Wlan device info.

---
+++ source/Net/CreativeCoders.Net.Avm/Tr064/Wlan/Responses/GetSpecificAssociatedDeviceInfoResponse.cs
@@ -0,0 +1,20 @@
+﻿using CreativeCoders.Net.Soap.Response;
+
+namespace CreativeCoders.Net.Avm.Tr064.Wlan.Responses
+{
+    [SoapResponse("GetSpecificAssociatedDeviceInfoResponse", "urn:dslforum-org:service:WLANConfiguration:1")]
+    public class GetSpecificAssociatedDeviceInfoResponse
+    {
+        [SoapResponseField("NewAssociatedDeviceIPAddress")]
+        public string IpAddress { get; set; }
+
+        [SoapResponseField("NewAssociatedDeviceAuthState")]
+        public string DeviceAuthState { get; set; }
+
+        [SoapResponseField("NewX_AVM-DE_Speed")]
+        public int Speed { get; set; }
+
+        [SoapResponseField("NewX_AVM-DE_SignalStrength")]
+        public int SignalStrength { get; set; }
+    }
+}
---
+++ source/Net/CreativeCoders.Net.Avm/Wlan.cs
@@ -0,0 +1,28 @@
+﻿using CreativeCoders.Net.Avm.Tr064;
+
+namespace CreativeCoders.Net.Avm
+{
+    public class Wlan
+    {
+        private readonly WlanApi _wlanApi;
+
+        public Wlan(string url, string userName, string password)
+        {
+            _wlanApi = new WlanApi(url, userName, password);
+        }
+
+        public WlanDeviceInfo GetWlanDeviceInfo(string macAddress)
+        {
+            var response = _wlanApi.GetSpecificAssociatedDeviceInfo(macAddress);
+            
+            return new WlanDeviceInfo
+            {
+                MacAddress = macAddress,
+                IpAddress = response.IpAddress,
+                DeviceAuthState = response.DeviceAuthState,
+                SignalStrength = response.SignalStrength,
+                Speed = response.Speed
+            };
+        }
+    }
+}
