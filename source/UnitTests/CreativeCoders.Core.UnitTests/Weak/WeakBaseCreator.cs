6865b1f2d51574ad219af571c023d65df704aca2
darthsharp <48331467+darthsharp@users.noreply.github.com>
2020-05-01 16:26:17 +0200

    Fix problem with to early released weak references

--- source/UnitTests/CreativeCoders.Core.UnitTests/Weak/WeakBaseCreator.cs
+++ source/UnitTests/CreativeCoders.Core.UnitTests/Weak/WeakBaseCreator.cs
@@ -11,5 +11,14 @@ namespace CreativeCoders.Core.UnitTests.Weak {
             var weakAction = new WeakBase<Action>(this, () => writer.Write("Test"), KeepOwnerAliveMode.NotKeepAlive);
             return weakAction;
         }
+
+        // ReSharper disable once MemberCanBeMadeStatic.Global
+        public WeakBase<Action> CreateWeakBaseWithoutOwner()
+        {
+            var writer = new TestConsoleWriter();
+            
+            var weakAction = new WeakBase<Action>(null, () => writer.Write("Test"), KeepOwnerAliveMode.NotKeepAlive);
+            return weakAction;
+        }
     }
 }
