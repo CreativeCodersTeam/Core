f244294a516fe01eb10a529e7c7aa28680cab595
darthsharp <48331467+darthsharp@users.noreply.github.com>
2021-09-24 20:54:32 +0200

    Add new project for localization

---
+++ source/Localization/CreativeCoders.Localization/DefaultExtendedStringLocalizer.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Localization;
+
+namespace CreativeCoders.Localization
+{
+    internal class DefaultExtendedStringLocalizer<T> : IExtendedStringLocalizer<T>
+    {
+        private readonly IStringLocalizer _globalLocalizer;
+
+        private readonly IStringLocalizer<T> _localizer;
+
+        public DefaultExtendedStringLocalizer(IStringLocalizer globalLocalizer, IStringLocalizer<T> localizer)
+        {
+            _globalLocalizer = globalLocalizer;
+            _localizer = localizer;
+        }
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+        {
+            var globalStrings = _globalLocalizer.GetAllStrings(includeParentCultures);
+
+            var strings = _localizer.GetAllStrings(includeParentCultures);
+
+            return MergeStrings(globalStrings, strings);
+        }
+
+        private static IEnumerable<LocalizedString> MergeStrings(IEnumerable<LocalizedString> globalStrings, IEnumerable<LocalizedString> strings)
+        {
+            var mergedStrings = new List<LocalizedString>(globalStrings);
+
+            foreach (var localizedString in strings)
+            {
+                var globalLocalizedString = mergedStrings.FirstOrDefault(x => x.Name == localizedString.Name);
+
+                if (globalLocalizedString == null)
+                {
+                    mergedStrings.Add(localizedString);
+
+                    continue;
+                }
+
+                if (globalLocalizedString.ResourceNotFound || !localizedString.ResourceNotFound)
+                {
+                    mergedStrings.Remove(globalLocalizedString);
+
+                    mergedStrings.Add(localizedString);
+                }
+            }
+
+            return mergedStrings;
+        }
+
+        public LocalizedString this[string name]
+        {
+            get
+            {
+                var text = _localizer[name];
+
+                if (text.ResourceNotFound)
+                {
+                    text = _globalLocalizer[name];
+                }
+
+                return text;
+            }
+        }
+
+        public LocalizedString this[string name, params object[] arguments]
+        {
+            get
+            {
+                var text = _localizer[name, arguments];
+
+                if (text.ResourceNotFound)
+                {
+                    text = _globalLocalizer[name, arguments];
+                }
+
+                return text;
+            }
+        }
+    }
+}
---
+++ source/Localization/CreativeCoders.Localization/DefaultStringLocalizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+
+namespace CreativeCoders.Localization
+{
+    internal class DefaultStringLocalizer : IStringLocalizer
+    {
+        private readonly IStringLocalizer _localizer;
+
+        public DefaultStringLocalizer(IStringLocalizerFactory factory, IOptions<ExtendedLocalizationOptions> options)
+        {
+            var location = new AssemblyName(
+                               options.Value.Assembly.FullName ??
+                               throw new ArgumentException("This assembly name not found")).Name ??
+                           throw new ArgumentException("This assembly name not found");
+
+            _localizer = factory.Create(options.Value.ResourceName, location);
+        }
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+            => _localizer.GetAllStrings(includeParentCultures);
+
+        public LocalizedString this[string name] => _localizer[name];
+
+        public LocalizedString this[string name, params object[] arguments] => _localizer[name, arguments];
+    }
+}
