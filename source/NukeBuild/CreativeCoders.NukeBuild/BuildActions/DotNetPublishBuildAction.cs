a21f9c754bf96b4ef26341e846b956125c52d9a2
darthsharp <48331467+darthsharp@users.noreply.github.com>
2022-03-24 19:48:27 +0100

    Add new nuke build action to publish a dotnet project

---
+++ source/NukeBuild/CreativeCoders.NukeBuild/BuildActions/DotNetPublishBuildAction.cs
@@ -0,0 +1,25 @@
+﻿using Nuke.Common.Tools.DotNet;
+
+namespace CreativeCoders.NukeBuild.BuildActions
+{
+    public class DotNetPublishBuildAction : BuildActionBase<DotNetPublishBuildAction>
+    {
+        private string _projectFile;
+
+        protected override void OnExecute()
+        {
+            DotNetTasks.DotNetPublish(x => x
+                .SetProject(_projectFile)
+                .SetConfiguration(BuildInfo.Configuration)
+                .SetOutput(BuildInfo.ArtifactsDirectory)
+                .SetVersion(BuildInfo.VersionInfo.NuGetVersionV2));
+        }
+
+        public DotNetPublishBuildAction AddProject(string projectFile)
+        {
+            _projectFile = projectFile;
+
+            return this;
+        }
+    }
+}
