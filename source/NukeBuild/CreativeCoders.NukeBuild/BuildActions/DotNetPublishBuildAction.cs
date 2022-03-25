b32c4715fc6eb64d49bb63fc3c6d7ef58548c942
darthsharp <48331467+darthsharp@users.noreply.github.com>
2022-03-25 21:16:56 +0100

    Pull request for feature/nukepublish (#26)
    * Add new nuke build action to publish a dotnet project
    
    * Output can be set for publish build action
    
    * Use new field for output
    
    * Opt out .NET and nuke telemetry

---
+++ source/NukeBuild/CreativeCoders.NukeBuild/BuildActions/DotNetPublishBuildAction.cs
@@ -0,0 +1,34 @@
+﻿using Nuke.Common.Tools.DotNet;
+
+namespace CreativeCoders.NukeBuild.BuildActions
+{
+    public class DotNetPublishBuildAction : BuildActionBase<DotNetPublishBuildAction>
+    {
+        private string _projectFile;
+
+        private string _output;
+
+        protected override void OnExecute()
+        {
+            DotNetTasks.DotNetPublish(x => x
+                .SetProject(_projectFile)
+                .SetConfiguration(BuildInfo.Configuration)
+                .SetOutput(_output)
+                .SetVersion(BuildInfo.VersionInfo.NuGetVersionV2));
+        }
+
+        public DotNetPublishBuildAction SetProject(string projectFile)
+        {
+            _projectFile = projectFile;
+
+            return this;
+        }
+
+        public DotNetPublishBuildAction SetOutput(string output)
+        {
+            _output = output;
+
+            return this;
+        }
+    }
+}
