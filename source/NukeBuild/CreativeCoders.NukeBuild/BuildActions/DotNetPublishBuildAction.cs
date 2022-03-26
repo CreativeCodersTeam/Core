785bec4fb5bae90315d844e31af1d71ff4c7b173
darthsharp <48331467+darthsharp@users.noreply.github.com>
2022-03-26 18:01:26 +0100

    Pass runtime id to publish dotnet and add self contained support

--- source/NukeBuild/CreativeCoders.NukeBuild/BuildActions/DotNetPublishBuildAction.cs
+++ source/NukeBuild/CreativeCoders.NukeBuild/BuildActions/DotNetPublishBuildAction.cs
@@ -8,13 +8,28 @@ namespace CreativeCoders.NukeBuild.BuildActions
 
         private string _output;
 
+        private string _runtime;
+
+        private bool _selfContained;
+
         protected override void OnExecute()
         {
-            DotNetTasks.DotNetPublish(x => x
-                .SetProject(_projectFile)
-                .SetConfiguration(BuildInfo.Configuration)
-                .SetOutput(_output)
-                .SetVersion(BuildInfo.VersionInfo.NuGetVersionV2));
+            DotNetTasks.DotNetPublish(x =>
+            {
+                var settings = x
+                    .SetProject(_projectFile)
+                    .SetConfiguration(BuildInfo.Configuration)
+                    .SetOutput(_output)
+                    .SetVersion(BuildInfo.VersionInfo.NuGetVersionV2)
+                    .SetSelfContained(_selfContained);
+
+                if (!string.IsNullOrWhiteSpace(_runtime))
+                {
+                    settings = settings.SetRuntime(_runtime);
+                }
+
+                return settings;
+            });
         }
 
         public DotNetPublishBuildAction SetProject(string projectFile)
@@ -27,6 +42,20 @@ namespace CreativeCoders.NukeBuild.BuildActions
         public DotNetPublishBuildAction SetOutput(string output)
         {
             _output = output;
+
+            return this;
+        }
+
+        public DotNetPublishBuildAction SetRuntime(string runtime)
+        {
+            _runtime = runtime;
+
+            return this;
+        }
+
+        public DotNetPublishBuildAction SetSelfContained(bool selfContained)
+        {
+            _selfContained = selfContained;
 
             return this;
         }
