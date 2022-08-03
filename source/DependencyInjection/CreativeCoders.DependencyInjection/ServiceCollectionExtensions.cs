36d1135ad68767a7d773d226822d39b82274f4a3
darthsharp <48331467+darthsharp@users.noreply.github.com>
2022-08-03 21:57:04 +0200

    Dependency registration via interface/classes (#29)

---
+++ source/DependencyInjection/CreativeCoders.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using CreativeCoders.Core.Collections;
+using CreativeCoders.Core.Reflection;
+using CreativeCoders.DependencyInjection.Registration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CreativeCoders.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static void ConfigureServicesFromAssembly(this IServiceCollection services, Assembly assembly)
+    {
+        var registrations = typeof(IServiceRegistration)
+            .GetImplementations(new[] {assembly})
+            .Select(x =>
+            {
+                if (Activator.CreateInstance(x) is not IServiceRegistration serviceRegistration)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return serviceRegistration;
+            });
+
+        registrations.ForEach(x => x.ConfigureServices(services));
+    }
+}
