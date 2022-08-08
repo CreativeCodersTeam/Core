23e179d39ded7c57b313ba7bc9a8b4bbe05dcef6
darthsharp <48331467+darthsharp@users.noreply.github.com>
2022-08-08 20:24:33 +0200

    Fix index out of bound if last option parameter has no value

---
+++ source/UnitTests/CreativeCoders.SysConsole.Cli.Parsing.UnitTests/TestData/TestOptionWithSingleBool.cs
@@ -0,0 +1,7 @@
+﻿namespace CreativeCoders.SysConsole.Cli.Parsing.UnitTests.TestData;
+
+public class TestOptionWithSingleBool
+{
+    [OptionParameter('c', "create")]
+    public bool CreateData { get; set; }
+}
