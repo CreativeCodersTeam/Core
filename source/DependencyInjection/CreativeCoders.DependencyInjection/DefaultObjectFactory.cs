0a72e8002ea4229805f854952d80e68a44d1db37
darthsharp <48331467+darthsharp@users.noreply.github.com>
2022-11-07 18:16:28 +0100

    Add none generic object factory

--- source/DependencyInjection/CreativeCoders.DependencyInjection/DefaultObjectFactory.cs
+++ source/DependencyInjection/CreativeCoders.DependencyInjection/DefaultObjectFactory.cs
@@ -4,6 +4,26 @@ namespace CreativeCoders.DependencyInjection;
 
 internal class DefaultObjectFactory<T> : IObjectFactory<T>
 {
+    private readonly IObjectFactory _objectFactory;
+
+    public DefaultObjectFactory(IObjectFactory objectFactory)
+    {
+        _objectFactory = Ensure.NotNull(objectFactory, nameof(objectFactory));
+    }
+
+    public T GetInstance()
+    {
+        return _objectFactory.GetInstance<T>();
+    }
+
+    public T CreateInstance(params object[] parameters)
+    {
+        return _objectFactory.CreateInstance<T>(parameters);
+    }
+}
+
+internal class DefaultObjectFactory : IObjectFactory
+{
     private readonly IServiceProvider _serviceProvider;
 
     public DefaultObjectFactory(IServiceProvider serviceProvider)
@@ -11,12 +31,12 @@ internal class DefaultObjectFactory<T> : IObjectFactory<T>
         _serviceProvider = Ensure.NotNull(serviceProvider, nameof(serviceProvider));
     }
 
-    public T GetInstance()
+    public T GetInstance<T>()
     {
         return _serviceProvider.GetServiceOrCreateInstance<T>();
     }
 
-    public T CreateInstance(params object[] parameters)
+    public T CreateInstance<T>(params object[] parameters)
     {
         return _serviceProvider.CreateInstance<T>(parameters);
     }
