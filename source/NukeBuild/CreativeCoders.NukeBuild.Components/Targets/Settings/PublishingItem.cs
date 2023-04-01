e14f0f8a764a4af24c8ee0dcf2b98b7e7a459a63
darthsharp <48331467+darthsharp@users.noreply.github.com>
2023-04-01 22:37:14 +0200

    New nuke target for publishing projects

---
+++ source/NukeBuild/CreativeCoders.NukeBuild.Components/Targets/Settings/PublishingItem.cs
@@ -0,0 +1,20 @@
+﻿using Nuke.Common.IO;
+
+namespace CreativeCoders.NukeBuild.Components.Targets.Settings;
+
+public class PublishingItem
+{
+    public PublishingItem(AbsolutePath projectPath, AbsolutePath outputPath)
+    {
+        ProjectPath = projectPath;
+        OutputPath = outputPath;
+    }
+
+    public AbsolutePath ProjectPath { get; }
+
+    public AbsolutePath OutputPath { get; }
+
+    public string? Runtime { get; set; }
+
+    public bool SelfContained { get; set; }
+}
