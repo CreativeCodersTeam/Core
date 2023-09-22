0438e274480036787a34735b1fff52b7b42d7523
darthsharp <48331467+darthsharp@users.noreply.github.com>
2023-09-22 20:58:10 +0200

    IAsyncDisposable implementation with delegating dispose (#45)
    Add new IAsyncDisposable implementation with a delegating async dispose

---
+++ source/UnitTests/CreativeCoders.Core.UnitTests/DelegateAsyncDisposableTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace CreativeCoders.Core.UnitTests;
+
+public class DelegateAsyncDisposableTests
+{
+    [Fact]
+    public void Ctor_NoDisposeCalled_DisposeActionNotCalled()
+    {
+        var actionCalled = false;
+
+        _ = new DelegateAsyncDisposable(() =>
+        {
+            actionCalled = true;
+
+            return ValueTask.CompletedTask;
+        }, true);
+
+        actionCalled
+            .Should()
+            .BeFalse();
+    }
+
+    [Fact]
+    public async Task Dispose_Once_DisposeActionIsCalled()
+    {
+        var actionCalled = false;
+
+        var d = new DelegateAsyncDisposable(() =>
+        {
+            actionCalled = true;
+
+            return ValueTask.CompletedTask;
+        }, true);
+
+        await d.DisposeAsync().ConfigureAwait(false);
+
+        actionCalled
+            .Should()
+            .BeTrue();
+    }
+
+    [Fact]
+    public async Task Dispose_Twice_DisposeActionIsCalledTwice()
+    {
+        var actionCalledCounter = 0;
+
+        var d = new DelegateAsyncDisposable(() =>
+        {
+            actionCalledCounter++;
+
+            return ValueTask.CompletedTask;
+        }, false);
+
+        await d.DisposeAsync().ConfigureAwait(false);
+        await d.DisposeAsync().ConfigureAwait(false);
+
+        actionCalledCounter
+            .Should()
+            .Be(2);
+    }
+
+    [Fact]
+    public async Task Dispose_TwiceWithOnlyDisposeOnceSet_DisposeActionIsCalledOnce()
+    {
+        var actionCalledCounter = 0;
+
+        var d = new DelegateAsyncDisposable(() =>
+        {
+            actionCalledCounter++;
+
+            return ValueTask.CompletedTask;
+        }, true);
+
+        await d.DisposeAsync().ConfigureAwait(false);
+        await d.DisposeAsync().ConfigureAwait(false);
+
+        actionCalledCounter
+            .Should()
+            .Be(1);
+    }
+}
