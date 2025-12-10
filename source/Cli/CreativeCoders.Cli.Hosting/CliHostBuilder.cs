2ab88211f0d7e812359b574ee0a3518b4e19deb1
darthsharp <48331467+darthsharp@users.noreply.github.com>
2025-12-10 18:24:41 +0100

    Add `[ExcludeFromCodeCoverage]` attribute to `CliHostBuilder`

--- source/Cli/CreativeCoders.Cli.Hosting/CliHostBuilder.cs
+++ source/Cli/CreativeCoders.Cli.Hosting/CliHostBuilder.cs
@@ -1,7 +1,8 @@
-using CreativeCoders.Cli.Hosting.Commands;
+using System.Diagnostics.CodeAnalysis;
 
 namespace CreativeCoders.Cli.Hosting;
 
+[ExcludeFromCodeCoverage]
 public static class CliHostBuilder
 {
     public static ICliHostBuilder Create()
