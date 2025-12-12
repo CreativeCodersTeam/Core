7e1842ac5bfb4b12716d2a1b7cbef07fedea7fe6
darthsharp <48331467+darthsharp@users.noreply.github.com>
2025-12-12 12:13:19 +0100

    Add unit tests for `DefaultCliHostBuilder` and `DefaultCliHost`:
    - Ensure proper registration and behavior of CLI host services (`HelpHandler`, `CommandValidator`, etc.).
    - Test help handling, command execution, and error scenarios in `DefaultCliHost`.

---
+++ tests/CreativeCoders.Cli.Tests/Hosting/DefaultCliHostBuilderTests.cs
@@ -0,0 +1,316 @@
+using System.Reflection;
+using AwesomeAssertions;
+using CreativeCoders.Cli.Core;
+using CreativeCoders.Cli.Hosting;
+using CreativeCoders.Cli.Hosting.Commands;
+using CreativeCoders.Cli.Hosting.Commands.Store;
+using CreativeCoders.Cli.Hosting.Commands.Validation;
+using CreativeCoders.Cli.Hosting.Help;
+using FakeItEasy;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace CreativeCoders.Cli.Tests.Hosting;
+
+public class DefaultCliHostBuilderTests
+{
+    [Fact]
+    public void Build_WithHelpEnabled_RegistersHelpHandlerAndSettings()
+    {
+        // Arrange
+        var builder = CreateBuilder();
+
+        builder.SkipScanEntryAssembly();
+        builder.EnableHelp(HelpCommandKind.Command);
+
+        var commandScanner = A.Fake<IAssemblyCommandScanner>();
+        var commandStore = A.Fake<ICliCommandStore>();
+        var validator = A.Fake<ICliCommandStructureValidator>();
+        var cliHost = A.Fake<ICliHost>();
+
+        A.CallTo(() => commandScanner.ScanForCommands(A<Assembly[]>.Ignored))
+            .Returns(CreateScanResult());
+
+        SubstituteServices(builder, commandScanner, commandStore, validator, cliHost);
+
+        var services = GetBuiltServiceProvider(builder);
+
+        // Act
+        var host = builder.Build();
+
+        // Assert
+        host
+            .Should()
+            .Be(cliHost);
+
+        services
+            .GetRequiredService<ICliCommandHelpHandler>()
+            .Should()
+            .BeOfType<CliCommandHelpHandler>();
+
+        services
+            .GetRequiredService<HelpHandlerSettings>()
+            .CommandKind
+            .Should()
+            .Be(HelpCommandKind.Command);
+    }
+
+    [Fact]
+    public void Build_WithHelpDisabled_RegistersDisabledHelpHandler()
+    {
+        // Arrange
+        var builder = CreateBuilder();
+
+        builder.SkipScanEntryAssembly();
+
+        var commandScanner = A.Fake<IAssemblyCommandScanner>();
+        var commandStore = A.Fake<ICliCommandStore>();
+        var validator = A.Fake<ICliCommandStructureValidator>();
+        var cliHost = A.Fake<ICliHost>();
+
+        A.CallTo(() => commandScanner.ScanForCommands(A<Assembly[]>.Ignored))
+            .Returns(CreateScanResult());
+
+        SubstituteServices(builder, commandScanner, commandStore, validator, cliHost);
+
+        var services = GetBuiltServiceProvider(builder);
+
+        // Act
+        var host = builder.Build();
+
+        // Assert
+        host
+            .Should()
+            .Be(cliHost);
+
+        services
+            .GetRequiredService<ICliCommandHelpHandler>()
+            .Should()
+            .BeOfType<DisabledCommandHelpHandler>();
+    }
+
+    [Fact]
+    public void Build_CallsValidatorWithCommandStore()
+    {
+        // Arrange
+        var builder = CreateBuilder();
+
+        builder.SkipScanEntryAssembly();
+
+        var commandScanner = A.Fake<IAssemblyCommandScanner>();
+        var commandStore = A.Fake<ICliCommandStore>();
+        var validator = A.Fake<ICliCommandStructureValidator>();
+        var cliHost = A.Fake<ICliHost>();
+
+        var commandInfos = new[]
+        {
+            new CliCommandInfo
+            {
+                CommandType = typeof(DefaultCliHostBuilderTests),
+                CommandAttribute = new CliCommandAttribute(["test"])
+            }
+        };
+
+        A.CallTo(() => commandScanner.ScanForCommands(A<Assembly[]>.Ignored))
+            .Returns(new AssemblyScanResult
+            {
+                CommandInfos = commandInfos,
+                GroupAttributes = []
+            });
+
+        SubstituteServices(builder, commandScanner, commandStore, validator, cliHost);
+
+        // Act
+        builder.Build();
+
+        // Assert
+        A.CallTo(() => validator.Validate(commandStore))
+            .MustHaveHappenedOnceExactly();
+
+        A.CallTo(() =>
+                commandStore.AddCommands(commandInfos, A<IEnumerable<CliCommandGroupAttribute>>.Ignored))
+            .MustHaveHappenedOnceExactly();
+    }
+
+    [Fact]
+    public void ScanAssemblies_AddsAssembliesToScanList()
+    {
+        // Arrange
+        var builder = CreateBuilder();
+
+        var assemblies = new[] { Assembly.GetExecutingAssembly(), typeof(DefaultCliHostBuilder).Assembly };
+
+        builder.SkipScanEntryAssembly();
+
+        var commandScanner = A.Fake<IAssemblyCommandScanner>();
+        var commandStore = A.Fake<ICliCommandStore>();
+        var validator = A.Fake<ICliCommandStructureValidator>();
+        var cliHost = A.Fake<ICliHost>();
+
+        A.CallTo(() => commandScanner.ScanForCommands(A<Assembly[]>.That.IsSameSequenceAs(assemblies)))
+            .Returns(new AssemblyScanResult
+            {
+                CommandInfos = [],
+                GroupAttributes = []
+            });
+
+        SubstituteServices(builder, commandScanner, commandStore, validator, cliHost);
+
+        // Act
+        builder.ScanAssemblies(assemblies).Build();
+
+        // Assert
+        A.CallTo(() => commandScanner.ScanForCommands(A<Assembly[]>.That.IsSameSequenceAs(assemblies)))
+            .MustHaveHappenedOnceExactly();
+    }
+
+    [Fact]
+    public void SkipScanEntryAssembly_True_DoesNotScanEntryAssembly()
+    {
+        // Arrange
+        var builder = CreateBuilder();
+
+        builder.SkipScanEntryAssembly();
+
+        var commandScanner = A.Fake<IAssemblyCommandScanner>();
+        var commandStore = A.Fake<ICliCommandStore>();
+        var validator = A.Fake<ICliCommandStructureValidator>();
+        var cliHost = A.Fake<ICliHost>();
+
+        A.CallTo(() => commandScanner.ScanForCommands(A<Assembly[]>.Ignored))
+            .Returns(new AssemblyScanResult
+            {
+                CommandInfos = Array.Empty<CliCommandInfo>(),
+                GroupAttributes = Array.Empty<CliCommandGroupAttribute>()
+            });
+
+        SubstituteServices(builder, commandScanner, commandStore, validator, cliHost);
+
+        // Act
+        builder.Build();
+
+        // Assert
+        A.CallTo(() => commandScanner.ScanForCommands(A<Assembly[]>.Ignored))
+            .MustHaveHappenedOnceExactly();
+    }
+
+    [Fact]
+    public void ConfigureServices_InvokesProvidedAction()
+    {
+        // Arrange
+        var builder = CreateBuilder();
+
+        builder.SkipScanEntryAssembly();
+
+        var wasCalled = false;
+
+        builder.ConfigureServices(_ => wasCalled = true);
+
+        var commandScanner = A.Fake<IAssemblyCommandScanner>();
+        var commandStore = A.Fake<ICliCommandStore>();
+        var validator = A.Fake<ICliCommandStructureValidator>();
+        var cliHost = A.Fake<ICliHost>();
+
+        A.CallTo(() => commandScanner.ScanForCommands(A<Assembly[]>.Ignored))
+            .Returns(CreateScanResult());
+
+        SubstituteServices(builder, commandScanner, commandStore, validator, cliHost);
+
+        // Act
+        builder.Build();
+
+        // Assert
+        wasCalled
+            .Should()
+            .BeTrue();
+    }
+
+    [Fact]
+    public void UseContext_AddsContextToServices()
+    {
+        // Arrange
+        var builder = CreateBuilder();
+
+        builder.SkipScanEntryAssembly();
+
+        builder.UseContext<DummyContext>(context => context.Value = 42);
+
+        var commandScanner = A.Fake<IAssemblyCommandScanner>();
+        var commandStore = A.Fake<ICliCommandStore>();
+        var validator = A.Fake<ICliCommandStructureValidator>();
+        var cliHost = A.Fake<ICliHost>();
+
+        A.CallTo(() => commandScanner.ScanForCommands(A<Assembly[]>.Ignored))
+            .Returns(CreateScanResult());
+
+        SubstituteServices(builder, commandScanner, commandStore, validator, cliHost);
+
+        var services = GetBuiltServiceProvider(builder);
+
+        // Act
+        builder.Build();
+
+        // Assert
+        var context = services.GetRequiredService<DummyContext>();
+
+        context
+            .Should()
+            .NotBeNull();
+
+        context.Value
+            .Should()
+            .Be(42);
+    }
+
+    private static DefaultCliHostBuilder CreateBuilder()
+    {
+        return new DefaultCliHostBuilder();
+    }
+
+    private static void SubstituteServices(
+        DefaultCliHostBuilder builder,
+        IAssemblyCommandScanner commandScanner,
+        ICliCommandStore commandStore,
+        ICliCommandStructureValidator validator,
+        ICliHost cliHost)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.AddSingleton(commandScanner);
+            services.AddSingleton(commandStore);
+            services.AddSingleton(validator);
+            services.AddSingleton(cliHost);
+        });
+    }
+
+    private static IServiceProvider GetBuiltServiceProvider(DefaultCliHostBuilder builder)
+    {
+        var method = typeof(DefaultCliHostBuilder)
+            .GetMethod("BuildServiceProvider", BindingFlags.Instance | BindingFlags.NonPublic)!;
+
+        return (IServiceProvider)method.Invoke(builder, Array.Empty<object>())!;
+    }
+
+    private static AssemblyScanResult CreateScanResult()
+    {
+        return new AssemblyScanResult
+        {
+            CommandInfos =
+            [
+                new CliCommandInfo
+                {
+                    CommandType = typeof(DefaultCliHostBuilderTests),
+                    CommandAttribute = new CliCommandAttribute(["test"])
+                }
+            ],
+            GroupAttributes = []
+        };
+    }
+
+    [UsedImplicitly]
+    private sealed class DummyContext : ICliCommandContext
+    {
+        public int Value { get; set; }
+    }
+}
---
+++ tests/CreativeCoders.Cli.Tests/Hosting/DefaultCliHostTests.cs
@@ -0,0 +1,197 @@
+using AwesomeAssertions;
+using CreativeCoders.Cli.Core;
+using CreativeCoders.Cli.Hosting;
+using CreativeCoders.Cli.Hosting.Commands;
+using CreativeCoders.Cli.Hosting.Commands.Store;
+using CreativeCoders.Cli.Hosting.Exceptions;
+using CreativeCoders.Cli.Hosting.Help;
+using FakeItEasy;
+using JetBrains.Annotations;
+using Spectre.Console;
+using Xunit;
+
+namespace CreativeCoders.Cli.Tests.Hosting;
+
+public class DefaultCliHostTests
+{
+    [Fact]
+    public async Task RunAsync_WhenHelpIsRequested_PrintsHelpAndReturnsSuccess()
+    {
+        // Arrange
+        var args = new[] { "help" };
+
+        var ansiConsole = A.Fake<IAnsiConsole>();
+        var commandStore = A.Fake<ICliCommandStore>();
+        var serviceProvider = A.Fake<IServiceProvider>();
+        var helpHandler = A.Fake<ICliCommandHelpHandler>();
+
+        A.CallTo(() => helpHandler.ShouldPrintHelp(args))
+            .Returns(true);
+
+        var host = new DefaultCliHost(ansiConsole, commandStore, serviceProvider, helpHandler);
+
+        // Act
+        var result = await host.RunAsync(args);
+
+        // Assert
+        A.CallTo(() => helpHandler.PrintHelp(args))
+            .MustHaveHappenedOnceExactly();
+
+        result.ExitCode
+            .Should()
+            .Be(CliExitCodes.Success);
+    }
+
+    [Fact]
+    public async Task RunAsync_WhenCommandNotFound_PrintsSuggestionsAndReturnsNotFound()
+    {
+        // Arrange
+        var args = new[] { "missing" };
+
+        var ansiConsole = A.Fake<IAnsiConsole>();
+        var commandStore = A.Fake<ICliCommandStore>();
+        var serviceProvider = A.Fake<IServiceProvider>();
+        var helpHandler = A.Fake<ICliCommandHelpHandler>();
+
+        A.CallTo(() => helpHandler.ShouldPrintHelp(args))
+            .Returns(false);
+
+        A.CallTo(() => commandStore.FindCommandNode(args))
+            .Returns(null);
+
+        var groupNode = new CliCommandGroupNode("missing", null);
+
+        A.CallTo(() => commandStore.FindCommandGroupNode(args))
+            .Returns(new FindCommandNodeResult<CliCommandGroupNode>(groupNode, []));
+
+        var host = new DefaultCliHost(ansiConsole, commandStore, serviceProvider, helpHandler);
+
+        // Act
+        var result = await host.RunAsync(args);
+
+        // Assert
+        result.ExitCode
+            .Should()
+            .Be(CliExitCodes.CommandNotFound);
+
+        A.CallTo(() => helpHandler.PrintHelpFor(A<IList<CliTreeNode>>.Ignored))
+            .MustHaveHappenedOnceExactly();
+    }
+
+    [Fact]
+    public async Task RunAsync_WhenCommandWithoutOptions_ExecutesAndReturnsResult()
+    {
+        // Arrange
+        var args = new[] { "run" };
+
+        var ansiConsole = A.Fake<IAnsiConsole>();
+        var commandStore = A.Fake<ICliCommandStore>();
+        var serviceProvider = A.Fake<IServiceProvider>();
+        var helpHandler = A.Fake<ICliCommandHelpHandler>();
+
+        A.CallTo(() => helpHandler.ShouldPrintHelp(args))
+            .Returns(false);
+
+        var commandInfo = new CliCommandInfo
+        {
+            CommandAttribute = new CliCommandAttribute(["run"]),
+            CommandType = typeof(DummyCommandWithResult)
+        };
+
+        var commandNode = new CliCommandNode(commandInfo, "run", null);
+
+        A.CallTo(() => commandStore.FindCommandNode(args))
+            .Returns(new FindCommandNodeResult<CliCommandNode>(commandNode, []));
+
+        A.CallTo(() => serviceProvider.GetService(typeof(int)))
+            .Returns(5);
+
+        var host = new DefaultCliHost(ansiConsole, commandStore, serviceProvider, helpHandler);
+
+        // Act
+        var result = await host.RunAsync(args);
+
+        // Assert
+        result.ExitCode
+            .Should()
+            .Be(5);
+    }
+
+    [Fact]
+    public async Task RunAsync_WhenCommandCreationFails_PrintsErrorAndReturnsExitCode()
+    {
+        // Arrange
+        var args = new[] { "run" };
+
+        var ansiConsole = A.Fake<IAnsiConsole>();
+        var commandStore = A.Fake<ICliCommandStore>();
+        var serviceProvider = A.Fake<IServiceProvider>();
+        var helpHandler = A.Fake<ICliCommandHelpHandler>();
+
+        A.CallTo(() => helpHandler.ShouldPrintHelp(args))
+            .Returns(false);
+
+        var commandInfo = new CliCommandInfo
+        {
+            CommandAttribute = new CliCommandAttribute(["run"]),
+            CommandType = typeof(FailingCommand)
+        };
+
+        var commandNode = new CliCommandNode(commandInfo, "run", null);
+
+        A.CallTo(() => commandStore.FindCommandNode(args))
+            .Returns(new FindCommandNodeResult<CliCommandNode>(commandNode, []));
+
+        var host = new DefaultCliHost(ansiConsole, commandStore, serviceProvider, helpHandler);
+
+        // Act
+        var result = await host.RunAsync(args);
+
+        // Assert
+        result.ExitCode
+            .Should()
+            .Be(CliExitCodes.CommandCreationFailed);
+    }
+
+    [UsedImplicitly]
+    private sealed class DummyCommand : ICliCommand
+    {
+        [UsedImplicitly]
+        public CommandResult? Result { get; set; }
+
+        public Task<CommandResult> ExecuteAsync()
+        {
+            return Task.FromResult(Result ?? new CommandResult());
+        }
+    }
+
+    private sealed class DummyCommandWithResult : ICliCommand
+    {
+        private readonly int _exitCode;
+
+        [UsedImplicitly]
+        public DummyCommandWithResult(int exitCode)
+        {
+            _exitCode = exitCode;
+        }
+
+        public Task<CommandResult> ExecuteAsync()
+        {
+            return Task.FromResult(new CommandResult { ExitCode = _exitCode });
+        }
+    }
+
+    private sealed class FailingCommand : ICliCommand
+    {
+        [UsedImplicitly]
+        public FailingCommand()
+        {
+            throw new InvalidOperationException("Failure in constructor");
+        }
+
+        public Task<CommandResult> ExecuteAsync()
+        {
+            return Task.FromResult(new CommandResult());
+        }
+    }
+}
