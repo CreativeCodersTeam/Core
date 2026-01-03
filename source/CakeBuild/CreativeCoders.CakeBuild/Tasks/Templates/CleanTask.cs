f2f42af96a85d8c3b55e3f85addd43206f07a052
darthsharp <48331467+darthsharp@users.noreply.github.com>
2026-01-03 12:13:50 +0100

    Enhance `CleanTask` to clean solution builds
    - Implement `RunAsync` method, invoking `DotNetClean` and deleting `bin`/`obj` directories.
    - Introduce private `DeleteDirectories` method to simplify directory cleanup logic with glob patterns and logging.

--- source/CakeBuild/CreativeCoders.CakeBuild/Tasks/Templates/CleanTask.cs
+++ source/CakeBuild/CreativeCoders.CakeBuild/Tasks/Templates/CleanTask.cs
@@ -1,3 +1,28 @@
-﻿namespace CreativeCoders.CakeBuild.Tasks.Templates;
+﻿using Cake.Common.Diagnostics;
+using Cake.Common.Tools.DotNet;
+using Cake.Core.IO;
 
-public class CleanTask<T> : FrostingTaskBase<T> where T : BuildContext { }
+namespace CreativeCoders.CakeBuild.Tasks.Templates;
+
+public class CleanTask<T> : FrostingTaskBase<T> where T : BuildContext
+{
+    public override async Task RunAsync(T context)
+    {
+        context.DotNetClean(context.SolutionFile.FullPath);
+
+        DeleteDirectories(context, context.RootDir.FullPath + "/**/bin");
+        DeleteDirectories(context, context.RootDir.FullPath + "/**/obj");
+    }
+
+    private static void DeleteDirectories(T context, string globPattern)
+    {
+        var dirs = context.Globber.Match(globPattern).OfType<DirectoryPath>();
+
+        foreach (var dir in dirs)
+        {
+            context.Information("Deleting directory '{0}'", dir);
+
+            context.FileSystem.GetDirectory(dir).Delete(true);
+        }
+    }
+}
