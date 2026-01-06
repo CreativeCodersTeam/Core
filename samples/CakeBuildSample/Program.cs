85c3aa2fa09788ceb0d642b97b0140c52216afde
darthsharp <48331467+darthsharp@users.noreply.github.com>
2026-01-06 13:47:28 +0100

    Update `ICodeCoverageTaskSettings` with .NET framework property and configure tool paths in `CakeBuildSample`
    - Added `Framework` property to `ICodeCoverageTaskSettings` for dynamic .NET version handling.
    - Configured `CakeHost` in `CakeBuildSample` to use a temporary tools directory and install required tools.

--- samples/CakeBuildSample/Program.cs
+++ samples/CakeBuildSample/Program.cs
@@ -1,4 +1,8 @@
-﻿using CreativeCoders.CakeBuild;
+﻿using Cake.Core.IO;
+using Cake.Frosting;
+using CreativeCoders.CakeBuild;
+using CreativeCoders.Core.IO;
+using CreativeCoders.Core.SysEnvironment;
 
 namespace CakeBuildSample;
 
@@ -10,6 +14,17 @@ internal static class Program
             .UseBuildContext<SampleBuildContext>()
             .AddDefaultTasks()
             .AddBuildServerIntegration()
+            .ConfigureHost(x =>
+            {
+                var tempToolsPath = FileSys.Path.Combine(FileSys.Path.GetTempPath(), ".cake-tools");
+
+                Console.WriteLine($"Using temp tools path: {tempToolsPath}");
+
+                x.SetToolPath(tempToolsPath);
+
+                x.InstallTool(new Uri("nuget:?package=GitVersion.Tool&version=6.5.1"));
+                x.InstallTool(new Uri("nuget:?package=dotnet-reportgenerator-globaltool&version=5.5.1"));
+            })
             .Build()
             .Run(args);
     }
--- source/CakeBuild/CreativeCoders.CakeBuild/Tasks/Templates/Settings/ICodeCoverageTaskSettings.cs
+++ source/CakeBuild/CreativeCoders.CakeBuild/Tasks/Templates/Settings/ICodeCoverageTaskSettings.cs
@@ -1,4 +1,9 @@
+using CreativeCoders.Core.SysEnvironment;
+
 namespace CreativeCoders.CakeBuild.Tasks.Templates.Settings;
 
 [CakeTaskSettings]
-public interface ICodeCoverageTaskSettings { }
+public interface ICodeCoverageTaskSettings
+{
+    string Framework => $"net{Env.Version.Major}.0";
+}
