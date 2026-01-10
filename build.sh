c2f49ba67e17b13adff39c0a3a1ed1d73c24e5f2
darthsharp <48331467+darthsharp@users.noreply.github.com>
2026-01-10 18:03:56 +0100

    Simplify build script by removing `pipefail` option in `set` command.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 bash --version 2>&1 | head -n 1
 
-set -eo pipefail
+set -e
 SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
 ###########################################################################
