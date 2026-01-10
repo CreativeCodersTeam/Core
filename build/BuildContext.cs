33691b94a32151d8e734bc50aaaecaaeea0c604e
darthsharp <48331467+darthsharp@users.noreply.github.com>
2026-01-10 17:36:16 +0100

    Replace Nuke build system with Cake.build and migrate corresponding build logic, settings, and configurations.

---
+++ build/BuildContext.cs
@@ -0,0 +1,33 @@
+using Cake.Common.Build;
+using Cake.Core;
+using Cake.Core.IO;
+using CreativeCoders.CakeBuild;
+using CreativeCoders.CakeBuild.Tasks.Defaults;
+using CreativeCoders.CakeBuild.Tasks.Templates.Settings;
+using CreativeCoders.Core;
+using CreativeCoders.Core.Collections;
+using JetBrains.Annotations;
+
+namespace Build;
+
+[UsedImplicitly]
+public class BuildContext(ICakeContext context) : CakeBuildContext(context), IDefaultTaskSettings
+{
+    public IList<DirectoryPath> DirectoriesToClean => this.CastAs<ICleanTaskSettings>()
+        .GetDefaultDirectoriesToClean().AddRange(RootDir.Combine(".tests"));
+
+
+    public string Copyright => $"{DateTime.Now.Year} CreativeCoders";
+
+    public string PackageProjectUrl => "https://github.com/CreativeCodersTeam/Core";
+
+    public string PackageLicenseExpression => PackageLicenseExpressions.ApacheLicense20;
+
+    public string NuGetFeedUrl => this.GitHubActions().Environment.Workflow.Workflow == "release"
+        ? "nuget.org"
+        : "https://nuget.pkg.github.com/CreativeCodersTeam/index.json";
+
+    public bool SkipPush => this.BuildSystem().IsPullRequest ||
+                            this.BuildSystem().IsLocalBuild ||
+                            this.GitHubActions().Environment.Runner.OS != "Linux";
+}
