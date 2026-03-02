0e47036bac9fecebdaad1f797662baaf0659e955
darthsharp <48331467+darthsharp@users.noreply.github.com>
2026-03-02 12:54:07 +0100

    feat: add configuration support with JSON file integration
    - Introduced `TryAddJsonFile` method in `ConfigurationBuilderExtensions` to conditionally add JSON configuration files.
    - Added new `CreativeCoders.Configuration` project to the solution with necessary dependencies.
    - Enhanced `ICliHostBuilder` with `UseConfiguration` method for customizable configuration setups.
    - Updated `Core.sln` and `CreativeCoders.Cli.Hosting` to incorporate new configuration capabilities.

---
+++ source/Configuration/CreativeCoders.Configuration/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using CreativeCoders.Core.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace CreativeCoders.Configuration;
+
+public static class ConfigurationBuilderExtensions
+{
+    public static IConfigurationBuilder TryAddJsonFile(this IConfigurationBuilder builder, string path)
+    {
+        return FileSys.File.Exists(path)
+            ? builder.AddJsonFile(path)
+            : builder;
+    }
+}
